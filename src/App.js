81e71b2ed5dd8b1d64c66dba46ce769f766b0bbc
Nick_edu <nick_edu@nameless.local>
2018-01-18 10:24:40 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header-background">
+          <h1 className="header-title">To-Do List</h1>
+        </header>
+       {/* To-do list goes here */}
+       {/* Error handling/error message */}
+      </div>
+    );
+  }
+}
+
+export default App;
